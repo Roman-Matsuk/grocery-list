8c55b7b2e6c5fa3bc98db3d6798b4bca72271aa9
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-02-05 20:35:13 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { GroceryList } from './GroceryList';
+import { ProductDetails } from './ProductDetails';
+
+const productsFromServer = [
+  { name: 'Bread', status: true, priority: 1, id: 1, time: new Date()},
+  { name: 'Milk', status: false, priority: 4, id: 2, time: new Date()},
+  { name: 'Ice cream', status: true, priority: 4, id: 3, time: new Date()},
+  { name: 'Potato', status: false, priority: 2, id: 4, time: new Date()},
+  { name: 'Cookies', status: true, priority: 3, id: 5, time: new Date()},
+  { name: 'Carrot', status: true, priority: 3, id: 6, time: new Date()},
+  { name: 'Tomato', status: false, priority: 5, id: 7, time: new Date()},
+  { name: 'Cake', status: true, priority: 5, id: 8, time: new Date()},
+  { name: 'Coke', status: true, priority: 1, id: 9, time: new Date()},
+  { name: 'Chocolate', status: false, priority: 2, id: 10, time: new Date()},
+]
 
 function App() {
+  const [products, setProducts] = useState(productsFromServer);
+  const [query, setQuery] = useState('All');
+  const [productToShow, setProductToShow] = useState({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Grocery List</h1>
+      <select
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="Ran out">Ran out</option>
+        <option value="Have">Have</option>
+      </select>
+      <GroceryList
+        products={products}
+        query={query}
+        setProductToShow={setProductToShow}
+      />
+      {productToShow.name
+        && (
+          <ProductDetails
+            productToShow={productToShow}
+            setProducts={setProducts}
+            setProductToShow={setProductToShow}
+          />
+        )
+      }
     </div>
   );
 }
